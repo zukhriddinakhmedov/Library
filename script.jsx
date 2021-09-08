d78cd7a2984814bd2d6cfd5614eccbda239c7190
Zukhriddin <zuxr.akhmedov@gmail.com>
2021-09-08 19:23:27 +0500

    Library app have implemented with script file which includes functionality

---
+++ script.jsx
@@ -0,0 +1,27 @@
+class Media {
+    constructor(title) {
+        this._title = title
+        this._isCheckedOut = false
+        this._ratings = []
+    }
+    get title() {
+        return this._title
+    }
+    get isCheckedOut() {
+        return this._isCheckedOut
+    }
+    get ratings() {
+        return this._ratings
+    }
+    set isCheckedOut(newIsCheckedOut) {
+        this._isCheckedOut = newIsCheckedOut
+    }
+    toggleCheckOutStatus() {
+        this._isCheckedOut = !this._isCheckedOut
+    }
+    //Below is the method which will get the average ratings of a product 
+    getAverageRating() {
+        let ratingsSum = this.ratings.reduce((currentSum, rating) =>
+            currentSum + rating, 0)
+    }
+}
