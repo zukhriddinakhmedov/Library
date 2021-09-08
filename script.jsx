75dd1d0fa0f99f822d616e7dd5635f60073e3277
Zukhriddin <zuxr.akhmedov@gmail.com>
2021-09-08 20:27:40 +0500

    tested successfully

--- script.jsx
+++ script.jsx
@@ -57,3 +57,18 @@ class Movie extends Media {
 }
 
 const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544)
+historyOfEverything.toggleCheckOutStatus()
+console.log(historyOfEverything.isCheckedOut)
+historyOfEverything.addRating(4, 5, 5)
+historyOfEverything.addRating(4, 5, 5)
+historyOfEverything.addRating(4, 5, 5)
+
+console.log(historyOfEverything.getAverageRating())
+
+const speed = new Movie('Jan de Bont', 'Speed', 116 + 'minutes')
+speed.toggleCheckOutStatus()
+console.log(speed.isCheckedOut)
+speed.addRating(1, 1, 5)
+speed.addRating(1, 1, 5)
+speed.addRating(1, 1, 5)
+console.log(speed.getAverageRating())
